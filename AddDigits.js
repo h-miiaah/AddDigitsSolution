4b357ba9c515873cac75fd07d25f790bc6152890
h-miiaah <haim.miiaah5@gmail.com>
2022-04-11 20:37:24 -0400

    comments

--- AddDigits.js
+++ AddDigits.js
@@ -9,5 +9,6 @@
  /**
   * First check if the number is less than 10.
   * If it is, then we return the number.
+  * If the number is greater than or equal to 10, then we calculate the sum of the digits.
   * 
   */
