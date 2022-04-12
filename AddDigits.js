f38ff9a72592f1948fc9066c0cc7bea0a79861a6
h-miiaah <haim.miiaah5@gmail.com>
2022-04-11 20:37:47 -0400

    comments

--- AddDigits.js
+++ AddDigits.js
@@ -11,5 +11,6 @@
   * If it is, then we return the number.
   * If the number is greater than or equal to 10, then we calculate the sum of the digits.
   * Then recursively call the addDigits function with the sum of the digits until we get a number less than 10.
+  * Once we get a number less than 10, we return that number.
   * 
   */
